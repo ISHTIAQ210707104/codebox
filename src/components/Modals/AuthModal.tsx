e3b0c7b04e16f478092d78719078a89133e3028f
Ishtiaq Ahmed <121130858+ISHTIAQ210707104@users.noreply.github.com>
2024-05-10 22:23:58 +0300

    Update AuthModal.tsx

--- src/components/Modals/AuthModal.tsx
+++ src/components/Modals/AuthModal.tsx
@@ -22,23 +22,23 @@ const AuthModal: React.FC<AuthModalProps> = () => {
     };
     window.addEventListener("keydown", handleEsc);
     return () => window.removeEventListener("keydown", handleEsc);
-  }, []);
+  }, [closeModal]); // Include closeModal in the dependency array
 
   return (
     <>
       {authModal.isOpen && (
         <>
           <div
-            className="absolute top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-60"
+            className="fixed top-0 left-0 w-full h-full flex items-center justify-center bg-black bg-opacity-60"
             onClick={closeModal}
           ></div>
-          <div className="w-full sm:w-[450px]  absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%]  flex justify-center items-center">
+          <div className="w-full sm:w-[450px] fixed top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex justify-center items-center">
             <div className="relative w-full h-full mx-auto flex items-center justify-center">
               <div className="bg-white rounded-lg shadow relative w-full bg-gradient-to-b from-brand-orange to-slate-900 mx-6">
                 <div className="flex justify-end p-2">
                   <button
                     type="button"
-                    className="bg-transparent  rounded-lg text-sm p-1.5 ml-auto inline-flex items-center hover:bg-gray-800 hover:text-white text-white"
+                    className="bg-transparent rounded-lg text-sm p-1.5 ml-auto inline-flex items-center hover:bg-gray-800 hover:text-white text-white"
                     onClick={closeModal}
                   >
                     <IoClose className="h-5 w-5" />
@@ -61,4 +61,3 @@ const AuthModal: React.FC<AuthModalProps> = () => {
 };
 
 export default AuthModal;
-
